5a4680807c250cff7a70df117b6f58d0b52dc657
Pallop Chaoputhipuchong <pallop.chao@gmail.com>
2019-07-29 11:14:00 +0700

    Make editor able to save in localstorage

--- src/pages/journal.js
+++ src/pages/journal.js
@@ -2,33 +2,43 @@ import React from 'react'
 import {Editor} from 'slate-react'
 import {Value} from 'slate'
 
-const initialValue = Value.fromJSON({
-  document: {
-    nodes: [
-      {
-        object: 'block',
-        type: 'paragraph',
-        nodes: [
-          {
-            object: 'text',
-            text: 'A line of text in a paragraph.',
-          },
-        ],
-      },
-    ],
+const existingValue = JSON.parse(localStorage.getItem('content'))
+
+const initialValue = Value.fromJSON(
+  existingValue || {
+    document: {
+      nodes: [
+        {
+          object: 'block',
+          type: 'paragraph',
+          nodes: [
+            {
+              object: 'text',
+              text: 'A line of text in a paragraph.',
+            },
+          ],
+        },
+      ],
+    },
   },
-})
+)
 
 export default function () {
   const [journalValue, setJournalValue] = React.useState(initialValue)
 
+  const onChange = ({value}) => {
+    if (value.document !== journalValue.document) {
+      const content = JSON.stringify(value.toJSON())
+      localStorage.setItem('content', content)
+    }
+
+    setJournalValue(value)
+  }
+
   return (
     <div>
       <h1>Hello Journal</h1>
-      <Editor
-        value={journalValue}
-        onChange={({value}) => setJournalValue(value)}
-      />
+      <Editor value={journalValue} onChange={onChange} />
     </div>
   )
 }
