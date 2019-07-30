39038aacea29efacee020c46521eafa54b659f23
Pallop Chaoputhipuchong <pallop.chao@gmail.com>
2019-07-30 16:08:40 +0700

    Make title editable

--- src/pages/journal.js
+++ src/pages/journal.js
@@ -2,23 +2,49 @@ import React from 'react'
 import {Editor as SlateEditor} from 'slate-react'
 import {Value} from 'slate'
 import styled from '@emotion/styled'
-import {Global, css} from '@emotion/core'
 import {getFirestore} from '../lib/firebase'
+import '../components/layout.css'
 import {useDebounce} from '../lib/utils'
+
+const USE_MOCK = false
+
+const TitleEditable = styled.input`
+  font-size: 2.25rem;
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+  min-height: 2.25rem;
+  line-height: 1.3;
+  cursor: text;
+  border: none;
+  font-weight: 500;
+  padding: 0;
+  width: 100%;
+`
+
+const Section = styled.div``
 
 const Layout = styled.div`
   max-width: 800px;
   margin: 0 auto;
 `
 const Editor = styled(SlateEditor)`
-  background: #fff;
-  padding: 1em;
-  margin: 1em -1em;
-  border-radius: 8px;
-  box-shadow: 0px 18px 15px -12px rgba(0, 0, 0, 0.03);
-`
+  cursor: text;
 
-const USE_MOCK = false
+  /* Fix differenciate height size between placeholder and actual content */
+  > * {
+    min-height: 1.6em;
+  }
+
+  ${props => props.isEmpty
+    && `
+    &::after {
+      content: '${props.customPlaceholder}';
+      display: block;
+      margin-top: -1.45em;
+      color: #a9a9a9;
+    }
+  `};
+`
 
 const saveToDB = async ({data, section}) => {
   const db = await getFirestore({mock: USE_MOCK})
@@ -75,6 +101,8 @@ const emptyValue = Value.fromJSON({
 export default function () {
   const [contentValue, setContentValue] = React.useState(emptyValue)
   const [keyTakeawaysValue, setKeyTakeawaysValue] = React.useState(emptyValue)
+  const [cover, setCover] = React.useState('')
+
   const debounceSaveSummary = useDebounce(
     () => saveToDB({section: 'SUMMARY', data: contentValue.toJSON()}),
     800,
@@ -83,50 +111,70 @@ export default function () {
     () => saveToDB({section: 'KEY_TAKEAWAYS', data: keyTakeawaysValue.toJSON()}),
     800,
   )
+  const debounceSaveTitle = useDebounce(
+    () => saveToDB({section: 'TITLE', data: cover}),
+    800,
+  )
 
   fetchDataAfterMounted({
-    onLoad: ({KEY_TAKEAWAYS, SUMMARY} = {}) => {
-      setContentValue(Value.fromJSON(SUMMARY))
-      setKeyTakeawaysValue(Value.fromJSON(KEY_TAKEAWAYS))
+    onLoad: ({KEY_TAKEAWAYS, SUMMARY, TITLE} = {}) => {
+      if (TITLE) {
+        setCover(TITLE)
+      }
+      if (SUMMARY) {
+        setContentValue(Value.fromJSON(SUMMARY))
+      }
+      if (KEY_TAKEAWAYS) {
+        setKeyTakeawaysValue(Value.fromJSON(KEY_TAKEAWAYS))
+      }
     },
   })
 
   const onContentChange = ({value}) => {
+    setContentValue(value)
     if (value.document !== contentValue.document) {
       debounceSaveSummary()
     }
-    setContentValue(value)
   }
   const onKeyTakeawaysChange = ({value}) => {
+    setKeyTakeawaysValue(value)
     if (value.document !== keyTakeawaysValue.document) {
       debounceSaveKeyTakeaways()
     }
-    setKeyTakeawaysValue(value)
+  }
+  const onTitleChange = (event) => {
+    setCover(event.target.value)
+    debounceSaveTitle()
   }
 
   return (
-    <Layout>
-      <Global
-        styles={css`
-          body {
-            background: #f9f9f9;
-          }
-          h1,
-          h2 {
-            margin-top: 1.2em;
-            margin-bottom: 0.3em;
-          }
-        `}
-      />
-      <h1>Title of the Book</h1>
-      <section>
-        <h2>Summary</h2>
-        <Editor value={contentValue} onChange={onContentChange} />
-      </section>
-      <section>
-        <h2>Key takeaways</h2>
-        <Editor value={keyTakeawaysValue} onChange={onKeyTakeawaysChange} />
-      </section>
-    </Layout>
+    <Section>
+      <Layout>
+        <TitleEditable
+          type="text"
+          placeholder="Book name"
+          onChange={onTitleChange}
+          value={cover}
+        />
+        <section>
+          <h2>Summary</h2>
+          <Editor
+            isEmpty={contentValue.document.text === ''}
+            customPlaceholder="What was the book about? Tell an amazing story that even 5 year-old child have to get excited."
+            value={contentValue}
+            onChange={onContentChange}
+          />
+        </section>
+        <section>
+          <h2>Key takeaways</h2>
+          <Editor
+            isEmpty={keyTakeawaysValue.document.text === ''}
+            customPlaceholder="What is the key takeaways for this book? Write down the bullet points or notes for key takeaways this will help yourself to remember the core concept of the book."
+            value={keyTakeawaysValue}
+            onChange={onKeyTakeawaysChange}
+          />
+        </section>
+      </Layout>
+    </Section>
   )
 }
